2567a30de45ea3195ec3ef6e3ab6c1f50bde5c80
Philippe Gagné <phgag@icloud.com>
2023-09-07 21:23:18 -0400

    prepare script to copy config files into Marlin.

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+ln -fF config/_Bootscreen.h Marlin/Marlin/
+ln -fF config/_Statusscreen.h Marlin/Marlin/
+ln -fF config/Configuration_adv.h Marlin/Marlin/
+ln -fF config/Configuration.h Marlin/Marlin/
+ln -fFs config/examples/ Marlin/config/
