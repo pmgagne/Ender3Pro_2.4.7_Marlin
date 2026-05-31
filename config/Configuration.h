f9157f9df8d9d73a30e74147a1a208f6f9d8d5c0
Philippe Gagné <philippemgagne@icloud.com>
2026-05-31 10:47:43 -0400

    Update nozzle probe offset and adjust prepare script for example configurations

--- config/Configuration.h
+++ config/Configuration.h
@@ -1514,7 +1514,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -16, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -7, -3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,11 @@
 
-ln -fF config/platformio.ini Marlin/Marlin/
+ln -fF config/platformio.ini Marlin/
 ln -fF config/_Bootscreen.h Marlin/Marlin/
 ln -fF config/_Statusscreen.h Marlin/Marlin/
 ln -fF config/Configuration_adv.h Marlin/Marlin/
 ln -fF config/Configuration.h Marlin/Marlin/
 ln -fFs config/examples/ Marlin/config/
+
+rm -f marlin/config/examples
+mkdir -p Marlin/config/examples/Creality/Ender-3\ Pro/CrealityV427
+cp -R Configurations/config/examples/Creality/Ender-3\ Pro/CrealityV427/ Marlin/config/examples/Creality/Ender-3\ Pro/CrealityV427
